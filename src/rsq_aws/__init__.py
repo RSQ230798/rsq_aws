b7a4df441c324470d77f11739014339ec2f576db
RSQ230798 <ryanstewartquigley@gmail.com>
2025-04-09 10:56:16 +0100

    cleaning

--- src/rsq_aws/__init__.py
+++ src/rsq_aws/__init__.py
@@ -12,10 +12,10 @@ Main Components:
 - BotoConnection: Interface for AWS Redshift operations using redshift-data API
 """
 
-from src.rsq_aws.s3.s3 import S3
-from src.rsq_aws.s3.path_factory import PathFactory
-from src.rsq_aws.redshift.boto_connection import BotoConnection
-from src.rsq_aws.redshift.psycopg_connection import PsycopgConnection
+from .s3.s3 import S3
+from .s3.path_factory import PathFactory
+from .redshift.boto_connection import BotoConnection
+from .redshift.psycopg_connection import PsycopgConnection
 
 __version__ = "1.0.0"
 
